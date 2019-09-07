e87dc2452783415b0353136cce982e5cda99ae39
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-09-07 19:42:19 +0200

    auth middleware

--- features/bikes/__init__.py
+++ features/bikes/__init__.py
@@ -1,10 +1,11 @@
 from aiohttp import web
+from middlewares.authMiddleware import authMiddleware
 from .bike_handler import BikeHandler
 
 
 def register_routes(app):
     handler = BikeHandler()
 
-    bike_app = web.Application()
+    bike_app = web.Application(middlewares=[authMiddleware])
     bike_app.add_routes([web.get('/search', handler.handle_search)])
     app.add_subapp('/bikes/', bike_app)
---
+++ middlewares/authMiddleware/__init__.py
@@ -0,0 +1,17 @@
+from aiohttp import web
+from aiohttp.web_exceptions import HTTPForbidden
+from firebase_admin import auth
+
+
+@web.middleware
+async def authMiddleware(request, handler):
+    authorization_header = request.headers.get('AUTHORIZATION')
+    if authorization_header is None:
+        raise HTTPForbidden()
+    token = authorization_header.split(' ')[-1]
+    try:
+        auth.verify_id_token(token)
+        response = await handler(request)
+        return response
+    except ValueError:
+        raise HTTPForbidden()
