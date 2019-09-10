6bdfd7edcf7ace7cc38fe6e59e17ef1a7e95386e
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2019-09-10 14:43:28 +0200

    introducing asyncio in cart dao

--- features/carts/cart_handler.py
+++ features/carts/cart_handler.py
@@ -17,11 +17,11 @@ class CartHandler(CartDao, ABC):
         if 'email' not in request.rel_url.query:
             return web.json_response({"error": "`email` query parameter must be provided"}, status=400)
 
-        cart = super().find_single_cart(request.rel_url.query["email"])
+        cart = await super().find_single_cart(request.rel_url.query["email"])
         if cart is None:
             return web.json_response({"error": "Cart not found"}, status=404)
 
-        items = super().get_items_from_cart(cart["cart_id"])
+        items = await super().get_items_from_cart(cart["cart_id"])
         return web.json_response({**cart, "bikes": items})
 
     @swagger_path("features/carts/swagger/add-to-cart.yaml")
@@ -35,21 +35,21 @@ class CartHandler(CartDao, ABC):
         cart_id = request.rel_url.query["cart_id"]
 
         # Check if bike exists
-        bike = super().get_single_bike(bike_id)
+        bike = await super().get_single_bike(bike_id)
         if bike is None:
             return web.json_response({"error": "No such bike document!"}, status=400)
 
         # Check if cart exists. TODO: Find cart by email. Ff cart does not exist then create one
-        cart = super().get_single_cart(cart_id)
+        cart = await super().get_single_cart(cart_id)
         if cart is None:
             return web.json_response({"error": "No such cart document!"}, status=400)
 
         # Check if an item is in the cart already
-        item = super().find_single_item_in_cart(cart_id, bike_id)
+        item = await super().find_single_item_in_cart(cart_id, bike_id)
         if item is not None:
             return web.json_response({"ok": False, "reason": "There is already a bike in the cart"}, status=200)
 
-        super().add_item_into_cart(cart_id, {"bike": Helpers.get_bike_ref_key(bike_id)})
+        await super().add_item_into_cart(cart_id, {"bike": Helpers.get_bike_ref_key(bike_id)})
 
         return web.json_response({"ok": True})
 
@@ -64,20 +64,20 @@ class CartHandler(CartDao, ABC):
         cart_id = request.rel_url.query["cart_id"]
 
         # Check if bike exists
-        bike = super().get_single_bike(bike_id)
+        bike = await super().get_single_bike(bike_id)
         if bike is None:
             return web.json_response({"error": "No such bike document!"}, status=400)
 
         # Check if cart exists. TODO: Find cart by email. Ff cart does not exist then create one
-        cart = super().get_single_cart(cart_id)
+        cart = await super().get_single_cart(cart_id)
         if cart is None:
             return web.json_response({"error": "No such cart document!"}, status=400)
 
         # Check if an item is in the cart
-        item = super().find_single_item_in_cart(cart_id, bike_id)
+        item = await super().find_single_item_in_cart(cart_id, bike_id)
         if item is None:
             return web.json_response({"ok": False, "reason": "There is no such bike in the cart"}, status=200)
 
-        super().delete_single_item_from_cart(cart_id, item["item_id"])
+        await super().delete_single_item_from_cart(cart_id, item["item_id"])
 
         return web.json_response({"ok": True})
--- features/carts/firebase_cart_dao.py
+++ features/carts/firebase_cart_dao.py
@@ -1,68 +1,78 @@
+import asyncio
+import concurrent.futures
 from google.cloud import firestore
 
 from constants import CollectionName
 from helpers import Helpers
 from .cart_dao import CartDao
 
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=20)
+
 
 class FirebaseCartDao(CartDao):
-    def get_single_cart(self, cart_id):
-        cart_ref = firestore.Client().collection(CollectionName.carts).document(cart_id).get()
+    async def get_single_cart(self, cart_id):
+        cart_ref_query = firestore.Client().collection(CollectionName.carts).document(cart_id)
+        cart_ref = await asyncio.get_running_loop().run_in_executor(executor, cart_ref_query.get)
         if cart_ref.exists:
             return {"cart_id": cart_ref.id, **cart_ref.to_dict()}
         return None
 
-    def get_single_bike(self, bike_id):
-        bike_ref = firestore.Client().collection(CollectionName.bikes).document(bike_id).get()
+    async def get_single_bike(self, bike_id):
+        bike_ref_query = firestore.Client().collection(CollectionName.bikes).document(bike_id)
+        bike_ref = await asyncio.get_running_loop().run_in_executor(executor, bike_ref_query.get)
         if bike_ref.exists:
             return {"bike_id": bike_ref.id, **bike_ref.to_dict()}
         return None
 
-    def get_items_from_cart(self, cart_id):
-        stream = firestore.Client() \
+    async def get_items_from_cart(self, cart_id):
+        query = firestore.Client() \
             .collection(CollectionName.carts) \
             .document(cart_id) \
-            .collection('items') \
-            .stream()
+            .collection('items')
+        stream = await asyncio.get_running_loop().run_in_executor(executor, query.stream)
         return [doc_item.to_dict() for doc_item in stream]
 
-    def find_single_cart(self, email):
+    async def find_single_cart(self, email):
         cart = None
-        stream = firestore.Client() \
+        query = firestore.Client() \
             .collection(CollectionName.carts) \
             .where('email', '==', email) \
-            .limit(1) \
-            .stream()
+            .limit(1)
+        stream = await asyncio.get_running_loop().run_in_executor(executor, query.stream)
         for doc in stream:
             cart = {"cart_id": doc.id, **doc.to_dict()}
         return cart
 
-    def find_single_item_in_cart(self, cart_id, bike_id):
+    async def find_single_item_in_cart(self, cart_id, bike_id):
         bike_key = Helpers.get_bike_ref_key(bike_id)
         item = None
-        stream = firestore.Client() \
+        query = firestore.Client() \
             .collection(CollectionName.carts) \
             .document(cart_id) \
             .collection('items') \
             .where('bike', '==', bike_key) \
-            .limit(1) \
-            .stream()
+            .limit(1)
+        stream = await asyncio.get_running_loop().run_in_executor(executor, query.stream)
         for doc in stream:
             item = {"item_id": doc.id, **doc.to_dict()}
         return item
 
-    def add_item_into_cart(self, cart_id, document):
-        firestore.Client() \
+    async def add_item_into_cart(self, cart_id, document):
+        doc = firestore.Client() \
             .collection(CollectionName.carts)\
             .document(cart_id)\
             .collection('items') \
-            .document() \
-            .set(document)
+            .document()
 
-    def delete_single_item_from_cart(self, cart_id, bike_id):
-        firestore.Client() \
+        def add_operation():
+            doc.set(document)
+
+        await asyncio.get_running_loop().run_in_executor(executor, add_operation)
+
+    async def delete_single_item_from_cart(self, cart_id, bike_id):
+        doc = firestore.Client() \
             .collection(CollectionName.carts) \
             .document(cart_id) \
             .collection('items') \
-            .document(bike_id) \
-            .delete()
+            .document(bike_id)
+        await asyncio.get_running_loop().run_in_executor(executor, doc.delete)
