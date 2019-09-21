297ee7b09f9555c6fd15f2ea19e2acb7964bb7b2
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-09-21 10:58:47 +0200

    generate service account json file

--- firebase_settings/__init__.py
+++ firebase_settings/__init__.py
@@ -1,9 +1,34 @@
+import json
+import os
+
 import firebase_admin
+from firebase_admin import credentials
+
+PRIVATE_KEY_VALUE = "PRIVATE_KEY_VALUE"
 
 
 def init():
     """Documentation about initialization of the Firebase application - https://firebase.google.com/docs/admin/setup"""
 
-    # set GOOGLE_APPLICATION_CREDENTIALS with a path to service account
-    # cred = credentials.Certificate('key.json')
-    firebase_admin.initialize_app(options={"databaseURL": "https://bikeshop-123f1.firebaseio.com"})
+    f = open("service-account.json", "w+")
+    output = json.dumps({
+        "type": "service_account",
+        "project_id": "bikeshop-123f1",
+        "private_key_id": "ebd0a338499c8e2b276e1e246f76cd22cad02ac3",
+        "private_key": PRIVATE_KEY_VALUE,
+        "client_email": "firebase-adminsdk-2ly68@bikeshop-123f1.iam.gserviceaccount.com",
+        "client_id": "104574911429780415218",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2ly68%40bikeshop-123f1.iam.gserviceaccount.com"
+    })
+    output = output.replace(PRIVATE_KEY_VALUE, os.getenv('BIKE_SHOP_SERVICE_ACCOUNT_PRIVATE_KEY'))
+    f.write(output)
+    f.close()
+
+    # or set GOOGLE_APPLICATION_CREDENTIALS with a path to service account instead of passing credential
+    firebase_admin.initialize_app(
+        options={"databaseURL": "https://bikeshop-123f1.firebaseio.com"},
+        credential=credentials.Certificate('service-account.json')
+    )
