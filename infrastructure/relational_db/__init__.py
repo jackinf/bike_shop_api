c53466a7e36f480dc216c6d23e10f2b8f1ac5e5a
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-10-11 22:11:29 +0200

    trying ssl

--- infrastructure/relational_db/__init__.py
+++ infrastructure/relational_db/__init__.py
@@ -6,7 +6,7 @@ if conn_string is None:
     db = PostgresqlDatabase('bikeshop', user='postgres', password='docker', host='localhost', port='5432')
 else:
     db_creds = conn_string.split(';')
-    db = PostgresqlDatabase(db_creds[0], user=db_creds[1], password=db_creds[2], host=db_creds[3], port=db_creds[4])
+    db = PostgresqlDatabase(db_creds[0], user=db_creds[1], password=db_creds[2], host=db_creds[3], port=db_creds[4], sslmode='require')
 
     # Another example is to use this:
     # db = connect('postgresql://postgres:******@localhost:5432/bikeshop')
