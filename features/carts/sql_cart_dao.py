40b361a2ba51c71fba42abde8ed3a477df03a5c8
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-11-02 15:04:10 +0100

    sql expressions for cart dao

--- features/carts/sql_cart_dao.py
+++ features/carts/sql_cart_dao.py
@@ -1,11 +1,10 @@
-import random
 from datetime import datetime
-from peewee import JOIN
+
+from exceptions import ItemNotFoundException
 from infrastructure.relational_db import db
-from infrastructure.relational_db.models import Bike, BikeType, User, BikeStatus, Cart
-from features.bikes.bike_dao import CartDao
+from infrastructure.relational_db.models import Bike, User, Cart
+from features.carts.cart_dao import CartDao
 from decorators.async_wrapper import async_wrapper
-from helpers import from_date_to_str
 
 class SqlCartDao(CartDao):
     def __enter__(self):
@@ -15,23 +14,58 @@ class SqlCartDao(CartDao):
     def __exit__(self, exc_type, exc_val, exc_tb):
         db.close()
 
-    async def get_single_cart(self, cart_id):
-        pass
+    @async_wrapper
+    def get_single_cart(self, user_id, bike_id):
+        single_cart_query = Cart \
+            .select() \
+            .where(Cart.user_id == user_id and Cart.bike_id == bike_id) \
+            .limit(1)
+        if len(single_cart_query) == 0:
+            raise ItemNotFoundException()
+        return single_cart_query[0]
 
-    async def get_single_bike(self, bike_id):  # TODO: Move to BikeDao
-        pass
+    @async_wrapper
+    def get_items_from_cart(self, user_id, bike_id):
+        get_items_query = Cart \
+            .select() \
+            .join(Bike) \
+            .join_from(Cart, User) \
+            .where(Cart.user_id == user_id and Cart.bike_id == bike_id)
+        bikes = [cart.bike_id for cart in get_items_query]
+        return bikes
 
-    async def get_items_from_cart(self, cart_id):
-        pass
+    @async_wrapper
+    def find_single_cart(self, email):
+        single_cart_query = Cart \
+            .select() \
+            .join(User) \
+            .where(Cart.user_id.email == email) \
+            .limit(1)
+        if len(single_cart_query) == 0:
+            raise ItemNotFoundException("cart")
+        return single_cart_query[0]
 
-    async def find_single_cart(self, email):
-        pass
+    @async_wrapper
+    def find_single_item_in_cart(self, cart_id, bike_id):
+        single_item_in_cart_query = Cart \
+            .select() \
+            .join(Bike) \
+            .where(Cart.id == cart_id and Cart.bike_id == bike_id) \
+            .limit(1)
+        if len(single_item_in_cart_query) == 0:
+            raise ItemNotFoundException("single item in cart")
+        return single_item_in_cart_query[0].bike_id
 
-    async def find_single_item_in_cart(self, items_in_cart, bike_key):
-        pass
+    @async_wrapper
+    def add_item_into_cart(self, email, bike_id):
+        if len(Bike.where(Bike.id == bike_id).limit(1)) == 0:
+            raise ItemNotFoundException("bike")
+        single_user_query = User.where(User.email == email).limit(1)
+        if len(single_user_query) == 0:
+            raise ItemNotFoundException("user")
+        user = single_user_query[0]
+        Cart.create(user_id=user.id, bike_id=bike_id, created_on=datetime.now(tz=None))
 
-    async def add_item_into_cart(self, cart_id, document):
-        pass
-
-    async def delete_single_item_from_cart(self, cart_id, bike_id):
-        pass
+    @async_wrapper
+    def delete_single_item_from_cart(self, cart_id):
+        Cart.delete_by_id(cart_id)
--- infrastructure/relational_db/sandbox-3-joins.py
+++ infrastructure/relational_db/sandbox-3-joins.py
@@ -1,5 +1,4 @@
 from pprint import pprint
-
 from peewee import JOIN
 
 from infrastructure.relational_db import db
@@ -9,16 +8,19 @@ db.connect()
 
 query_01 = Bike \
     .select() \
-    .join_from(Bike, Cart, JOIN.LEFT_OUTER) \
-    .join(User) \
-    .where(Cart.id != None and Cart.user_id.email != 'zeka.rum@gmail.com')
+    .join(Cart, JOIN.LEFT_OUTER) \
+    .where(Cart.id != None)
 print(query_01.sql())
 pprint(len(query_01))
 
-# query_02 = Cart \
-#     .select() \
-#     .join_from(Cart, Bike, JOIN.LEFT_OUTER) \
-#     .join_from(Cart, User, JOIN.LEFT_OUTER) \
-#     .where()
+
+query_02 = Cart \
+    .select() \
+    .join(Bike) \
+    .join_from(Cart, User) \
+    .where(Cart.user_id.email == "zeka.rum@gmail.com")
+print(query_02.sql())
+for res in query_02:
+    pprint(res.bike_id)
 
 db.close()
